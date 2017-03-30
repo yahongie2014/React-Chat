2c6fa48c276275341f07e2214e8596ee903270fa
Dev Ahmed Saeed <ahmed_saee373@yahoo.com>
2017-03-31 01:15:52 +0200

    help

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
  */
 
 var express = require('express');
+var app = express();
 var http = require('http');
 
 var socket = require('./routes/socket.js');
@@ -12,6 +13,7 @@ var server = http.createServer(app);
 
 /* Configuration */
 app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
 app.use(express.static(__dirname + '/public'));
 app.set('port', 5000);
 app.get('/', function(request, response) {
@@ -24,7 +26,7 @@ io.sockets.on('connection', socket);
 
 /* Start server */
 server.listen(app.get('port'), function (){
-  console.log('Express server listening on port %d in %s mode', app.get('port'));
+  console.log('Node app is running on port', app.get('port'));
 });
 
 module.exports = app;
