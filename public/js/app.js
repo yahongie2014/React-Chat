7eed10fc43d71f254835a493d2f6b363ca849b64
Dev Ahmed Saeed <ahmed_saee373@yahoo.com>
2017-03-31 00:34:42 +0200

    v 1.5
    dsign update

--- public/js/app.js
+++ public/js/app.js
@@ -10,16 +10,16 @@ var UsersList = React.createClass({
 
 	render: function render() {
 		return React.createElement(
-			'div',
-			{ className: 'text-center' },
+    		'div',
+			{ className: 'mg-md text-center fadeInUp animDelay04 tc-cerulean-blue animated' },
 			React.createElement(
 				'h3',
-            { className: 'mg-md text-center  mt40 tc-black fadeInUp animDelay06 animated' },
+			{ className: 'btn btn-rd btn-wire btn-sm navbar-nav-li-a wire-btn-cerulean-blue contactme' },
 				' Online Users '
 			),
 			React.createElement(
 				'ul',
-				null,
+			{ className: 'mg-lg fadeInLeft animDelay06  tc-battleship-grey animated' },
 				this.props.users.map(function (user, i) {
 					return React.createElement(
 						'li',
@@ -33,21 +33,21 @@ var UsersList = React.createClass({
 });
 
 var Message = React.createClass({
-	displayName: 'Message',
+	displayName: 'bloc bgc-anti-flash-white l-bloc',
 
 	render: function render() {
 		return React.createElement(
 			'div',
-			{ className: 'message' },
+			{ className: 'bloc bgc-anti-flash-white l-bloc' },
 			React.createElement(
 				'strong',
-				null,
+				{ className: 'mg-md  mb50 tc-cerulean-blue text-center' },
 				this.props.user,
-				' :'
+				':'
 			),
 			React.createElement(
 				'span',
-				null,
+				{ className: 'mg-md  mb50 tc-cerulean-blue text-center' },
 				this.props.text
 			)
 		);
@@ -60,12 +60,11 @@ var MessageList = React.createClass({
 	render: function render() {
 		return React.createElement(
 			'div',
-			{ className: 'tc-battleship-grey' },
-			React.createElement(
-			'br',
-			{ className: 'btn btn-wire btn-rd button-hire btn-xl wire-btn-cerulean-blue contactme' },
-
+			{ className: 'wrapper' },
 			),
+			React.createElement(
+			'div',
+			{ className: 'tc-battleship-grey' },
 			React.createElement(
 				'h2',
 			{ className: 'btn btn-wire btn-rd button-hire btn-xl wire-btn-cerulean-blue contactme' },
@@ -106,7 +105,7 @@ var MessageForm = React.createClass({
 	render: function render() {
 		return React.createElement(
 			'div',
-			{ className: 'mg-md text-center fadeInUp animDelay04 tc-cerulean-blue animated' },
+			{ className: 'btn btn-rd btn-wire btn-sm navbar-nav-li-a wire-btn-cerulean-blue contactme' },
 			React.createElement(
 				'h3',
 			{ className: 'mg-md text-center fadeInUp animDelay04 tc-cerulean-blue animated' },
@@ -200,7 +199,7 @@ var ChatApp = React.createClass({
 
 		users.push(name);
 		messages.push({
-			user: 'APPLICATION BOT',
+			user: 'User',
 			text: name + ' Joined'
 		});
 		this.setState({ users: users, messages: messages });
@@ -215,7 +214,7 @@ var ChatApp = React.createClass({
 		var index = users.indexOf(name);
 		users.splice(index, 1);
 		messages.push({
-			user: 'APPLICATION BOT',
+			user: 'User',
 			text: name + ' Left'
 		});
 		this.setState({ users: users, messages: messages });
@@ -232,7 +231,7 @@ var ChatApp = React.createClass({
 		users.splice(index, 1, newName);
 		messages.push({
 			user: 'APPLICATION BOT',
-			text: 'Change Name : ' + oldName + ' ==> ' + newName
+			text: 'Change Name : ' + oldName + ' ' + newName
 		});
 		this.setState({ users: users, messages: messages });
 	},
@@ -2049,27 +2048,10 @@ var DOMPropertyInjection = {
 };
 var defaultValueCache = {};
 
-/**
- * DOMProperty exports lookup objects that can be used like functions:
- *
- *   > DOMProperty.isValid['id']
- *   true
- *   > DOMProperty.isValid['foobar']
- *   undefined
- *
- * Although this may be confusing, it performs better in general.
- *
- * @see http://jsperf.com/key-exists
- * @see http://jsperf.com/key-missing
- */
 var DOMProperty = {
 
   ID_ATTRIBUTE_NAME: 'data-reactid',
 
-  /**
-   * Checks whether a property name is a standard property.
-   * @type {Object}
-   */
   isStandardName: {},
 
   /**
@@ -17069,68 +17051,6 @@ module.exports = SyntheticWheelEvent;
 'use strict';
 
 var invariant = require("./invariant");
-
-/**
- * `Transaction` creates a black box that is able to wrap any method such that
- * certain invariants are maintained before and after the method is invoked
- * (Even if an exception is thrown while invoking the wrapped method). Whoever
- * instantiates a transaction can provide enforcers of the invariants at
- * creation time. The `Transaction` class itself will supply one additional
- * automatic invariant for you - the invariant that any transaction instance
- * should not be run while it is already being run. You would typically create a
- * single instance of a `Transaction` for reuse multiple times, that potentially
- * is used to wrap several different methods. Wrappers are extremely simple -
- * they only require implementing two methods.
- *
- * <pre>
- *                       wrappers (injected at creation time)
- *                                      +        +
- *                                      |        |
- *                    +-----------------|--------|--------------+
- *                    |                 v        |              |
- *                    |      +---------------+   |              |
- *                    |   +--|    wrapper1   |---|----+         |
- *                    |   |  +---------------+   v    |         |
- *                    |   |          +-------------+  |         |
- *                    |   |     +----|   wrapper2  |--------+   |
- *                    |   |     |    +-------------+  |     |   |
- *                    |   |     |                     |     |   |
- *                    |   v     v                     v     v   | wrapper
- *                    | +---+ +---+   +---------+   +---+ +---+ | invariants
- * perform(anyMethod) | |   | |   |   |         |   |   | |   | | maintained
- * +----------------->|-|---|-|---|-->|anyMethod|---|---|-|---|-|-------->
- *                    | |   | |   |   |         |   |   | |   | |
- *                    | |   | |   |   |         |   |   | |   | |
- *                    | |   | |   |   |         |   |   | |   | |
- *                    | +---+ +---+   +---------+   +---+ +---+ |
- *                    |  initialize                    close    |
- *                    +-----------------------------------------+
- * </pre>
- *
- * Use cases:
- * - Preserving the input selection ranges before/after reconciliation.
- *   Restoring selection even in the event of an unexpected error.
- * - Deactivating events while rearranging the DOM, preventing blurs/focuses,
- *   while guaranteeing that afterwards, the event system is reactivated.
- * - Flushing a queue of collected DOM mutations to the main UI thread after a
- *   reconciliation takes place in a worker thread.
- * - Invoking any collected `componentDidUpdate` callbacks after rendering new
- *   content.
- * - (Future use case): Wrapping particular flushes of the `ReactWorker` queue
- *   to preserve the `scrollTop` (an automatic scroll aware DOM).
- * - (Future use case): Layout calculations before and after DOM updates.
- *
- * Transactional plugin API:
- * - A module that has an `initialize` method that returns any precomputation.
- * - and a `close` method that accepts the precomputation. `close` is invoked
- *   when the wrapped process is completed, or has failed.
- *
- * @param {Array<TransactionalWrapper>} transactionWrapper Wrapper modules
- * that implement `initialize` and `close`.
- * @return {Transaction} Single transaction for reuse in thread.
- *
- * @class Transaction
- */
 var Mixin = {
   /**
    * Sets up this instance so that it is prepared for collecting metrics. Does
@@ -17160,18 +17080,6 @@ var Mixin = {
   isInTransaction: function() {
     return !!this._isInTransaction;
   },
-
-  /**
-   * Executes the function within a safety window. Use this for the top level
-   * methods that result in large amounts of computation/mutations that would
-   * need to be safety checked.
-   *
-   * @param {function} method Member of scope to call.
-   * @param {Object} scope Scope to invoke from.
-   * @param {Object?=} args... Arguments to pass to the method (optional).
-   *                           Helps prevent need to bind in many cases.
-   * @return Return value from `method`.
-   */
   perform: function(method, scope, a, b, c, d, e, f) {
     ("production" !== process.env.NODE_ENV ? invariant(
       !this.isInTransaction(),
@@ -18952,29 +18860,7 @@ module.exports = instantiateReactComponent;
 }).call(this,require('_process'))
 },{"./Object.assign":28,"./ReactCompositeComponent":39,"./ReactEmptyComponent":61,"./ReactNativeComponent":75,"./invariant":137,"./warning":156,"_process":2}],137:[function(require,module,exports){
 (function (process){
-/**
- * Copyright 2013-2015, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- *
- * @providesModule invariant
- */
-
 "use strict";
-
-/**
- * Use invariant() to assert state which your program assumes to be true.
- *
- * Provide sprintf-style format (only %s is supported) and arguments
- * to provide information about what broke and what you were
- * expecting.
- *
- * The invariant message will be stripped in production, but the invariant
- * will remain to ensure logic does not differ in production.
- */
 
 var invariant = function(condition, format, a, b, c, d, e, f) {
   if ("production" !== process.env.NODE_ENV) {
